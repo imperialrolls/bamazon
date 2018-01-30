1962d562ded1cacc1f4a8a62e024704fa59ce2ac
Christopher Rolls <imperialrolls@gmail.com>
2018-01-30 15:09:00 -0700

    database query and basic display function added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,9 +18,20 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  connection.end();
+  queryAllProducts();
+//  connection.end();
 });
 
+
+function queryAllProducts() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if(err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+    }
+    console.log("-----------------------------------");
+  });
+}
 
 
 
