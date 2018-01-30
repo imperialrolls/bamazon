86e320a2775c3d18b4062c80948bd5ab94993b70
Christopher Rolls <imperialrolls@gmail.com>
2018-01-30 14:40:47 -0700

    database connection function added to js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,14 +1,51 @@
-// Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+// First we establish our database connection via mysql
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // username
+  user: "root",
+
+  // password
+  password: "",
+  database: "bamazon"
+});
+
+// console log connection ID
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
+});
+
+
+
+
+
 // The app should then prompt users with two messages.
+
+
 
 // The first should ask them the ID of the product they would like to buy.
 // The second message should ask how many units of the product they would like to buy.
 
+
+
 // Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+
 
 // If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
 
+
+
 // However, if your store does have enough of the product, you should fulfill the customer's order.
+
+
+
 
 // This means updating the SQL database to reflect the remaining quantity.
 // Once the update goes through, show the customer the total cost of their purchase.
