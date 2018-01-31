e715700b1382e323a97505707a9298308cd3b5e3
Christopher Rolls <imperialrolls@gmail.com>
2018-01-30 19:18:04 -0700

    display table finalized

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,42 +1,124 @@
-// First we establish our database connection via mysql
+// list of requirements
 
 var mysql = require("mysql");
+var inquirer = require('inquirer');
+var Table = require('cli-table');
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-
-  // username
   user: "root",
-
-  // password
   password: "",
   database: "bamazon"
 });
 
-// console log connection ID
+// connect to database and console log connection id
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  queryAllProducts();
-//  connection.end();
+  	readProducts();
+  	checkAndBuy2();
+	connection.end();
 });
 
-
-function queryAllProducts() {
+function readProducts() {
+  console.log("Selecting all products...\n");
   connection.query("SELECT * FROM products", function(err, res) {
-    if(err) throw err;
+    // define  
+        var table = new Table({
+            head: ['ID', 'Product Name', 'Department', 'Price', 'Stock Quantity']
+        });
+
+        //
+        console.log("Do you like what you see?: ");
+        console.log("===========================================");
+
+    // here we push data from the 'product' database table into the display table
     for (var i = 0; i < res.length; i++) {
-      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+    	table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
     }
     console.log("-----------------------------------");
+    console.log(table.toString());
   });
+
+
+// The app should then prompt users with two messages.
+ inquirer.prompt([{
+
+            name: "itemId",
+            type: "input",
+            message: "See something you like? Just enter its ID! ",
+
+            validate: function(value) {
+                if (isNaN(value) == false) {
+                 	return true;
+                } else {
+                    return false;
+                }
+            }
+        }, {
+            name: "Quantity",
+            type: "input",
+            message: "How many of this item would you like to buy?",
+
+            validate: function(value) {
+                if (isNaN(value) == false) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+        }]).then(function(answer) {
+
+            var chosenId = answer.itemId - 1
+
+            var chosenProduct = res[chosenId]
+
+            var chosenQuantity = answer.Quantity
+
+            if (chosenQuantity < res[chosenId].StockQuantity) {
+
+                console.log("Your total for " + "(" + answer.Quantity + ")" + " - " + res[chosenId].ProductName + " is: " + res[chosenId].Price.toFixed(2) * chosenQuantity);
+
+                connection.query("UPDATE products SET ? WHERE ?", [{
+
+                    StockQuantity: res[chosenId].StockQuantity - chosenQuantity
+
+                }, {
+
+                    id: res[chosenId].id
+
+                }], function(err, res) {
+
+                    //console.log(err);
+
+                    checkAndBuy2();
+
+                });
+
+
+
+            } else {
+
+                console.log("Sorry, insufficient Quanity at this time. All we have is " + res[chosenId].StockQuantity + " in our Inventory.");
+
+                checkAndBuy2();
+
+
+            }
+
+        })
+
+    })
+
 }
 
 
 
 
-// The app should then prompt users with two messages.
+// checkAndBuy2();
+
 
 
 
