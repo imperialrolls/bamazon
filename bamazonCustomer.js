1b0b229cd333b8d1af955581860bf0c8ce1513eb
Christopher Rolls <imperialrolls@gmail.com>
2018-01-31 07:35:43 -0700

    first assignment complete - code clean-up

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,33 @@
+// Challenge #1: Customer View (Minimum Requirement)
+
+// Create a MySQL Database called bamazon.
+// Then create a Table inside of that database called products.
+// The products table should have each of the following columns:
+
+// item_id (unique id for each product)
+// product_name (Name of product)
+// department_name
+// price (cost to customer)
+// stock_quantity (how much of the product is available in stores)
+
+// Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
+// Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+// The app should then prompt users with two messages.
+
+// The first should ask them the ID of the product they would like to buy.
+// The second message should ask how many units of the product they would like to buy.
+
+// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+
+// However, if your store does have enough of the product, you should fulfill the customer's order.
+
+// This means updating the SQL database to reflect the remaining quantity.
+// Once the update goes through, show the customer the total cost of their purchase.
+
+// ============================================================================================================
+
 // list of requirements
 
 var mysql = require("mysql");
@@ -20,16 +50,17 @@ connection.connect(function(err) {
 	// connection.end();
 });
 
+	
 function readProducts() {
   console.log("Selecting all products...\n");
   connection.query("SELECT * FROM products", function(err, res) {
-    // define  
+// define a new table which will display the results of our database query
         var table = new Table({
             head: ['ID', 'Product Name', 'Department', 'Price', 'Stock Quantity']
         });
         console.log("===========================================");
 
-    // here we push data from the 'product' database table into the display table
+// here we push data from the 'product' database table into the display table
     for (var i = 0; i < res.length; i++) {
     	table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
     }
@@ -41,19 +72,20 @@ function readProducts() {
 }
 
 
+// let's use inquirer to ask for the ID of the product the customer would like to buy.
 function productSearch() {
   inquirer.prompt([
     {
       name: "item_id",
       type: "choices",
-      message: "Welcome to Bamazon! What is the id of the product you would like to buy?",
+      message: "Hello consumer! What is the id of the product you would like to purchase?",
       validate: function (value) {
         if (isNaN(value) === false) {
-          return true;
-        }
-        console.log("Please enter a item ID!");
-        return false;
-      }
+        	return true;
+        	}
+        	console.log("Attention shopper! Don't forget the ID.");
+        	return false;
+      	}
     },
     {
       name: "stock",
@@ -63,12 +95,12 @@ function productSearch() {
         if (isNaN(value) === false) {
           return true;
         }
-        console.log("Please enter a item ID!");
+        console.log("Attention shopper! How many would you like?");
         return false;
       }
     }
   ]).then(function (answers) {
-    //variables that stores user inputs from question prompt
+    //define local variables to store input values from prompts
     let id = answers.item_id;
     let qty = answers.stock;
     updateStock();
@@ -83,8 +115,9 @@ function productSearch() {
         if (quantity >= answers.stock) {
           quantity = quantity - answers.stock;
           console.log(`${quantity} remaining.`);
+        // this will display the total price of purchased items
           var total = (answers.stock * price);
-          console.log(`Thanks for purchasing! Your total is $${total}`);
+          console.log(`Thanks for shopping! Your total is $${total}`);
           connection.query(queryUpdate, [{ stock_quantity: quantity }, { item_id: id }], function (err) {
             if (err) throw err;
           })
@@ -96,33 +129,6 @@ function productSearch() {
     }
   })
 }
-
-
-
-
-
-
-
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-
-
-
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-
-
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-
-
-
-// However, if your store does have enough of the product, you should fulfill the customer's order.
-
-
-
-
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase.
 
 
 
